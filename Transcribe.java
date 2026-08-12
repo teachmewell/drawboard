671c60916ce2d5f293c8526a6bce3669315a38d7
teachmewell <coyu3@duck.com>
2026-08-12 15:08:17 +0200

    Update Transcribe.java

--- Transcribe.java
+++ Transcribe.java
@@ -1,9 +1,9 @@
 public class Transcribe{
-int[][] a;
+
 
 public static void main(String[] args){
 
-a = IntoFile.loadImage("drawn.png");
+int[][] a = IntoFile.loadImage("drawn.png");
 Predefined.intoText(a);
 
 }
