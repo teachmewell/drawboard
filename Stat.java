17729b40a3ed07b081aa382aa64c46cb87b5e3ca
teachmewell <coyu3@duck.com>
2026-08-12 17:28:10 +0200

    Update Stat.java

--- Stat.java
+++ Stat.java
@@ -10,12 +10,32 @@ for(int x = 0; x< pic.length; x++){
   }
 }
 } 
-    }
+    return a;}
 
   //______________________________________________________________________________________________________________________________
 
    public static void main(String[] args){
        int[][] a = IntoFile.loadImage("drawn.png");
+      System.out.println("Enlarge by how much? (one bit should become how many bits)");
+      boolean loo=true;
+    while(loo){
+      System.out.println("Only input numbers 0-9 . ");
+      
+      int input = System.in.read();
+      switch(input){
+         case '0': input = input -'0'; loo=false; break; 
+         case '1': input = input -'0'; loo=false; break; 
+         case '2': input = input -'0'; loo=false; break; 
+         case '3': input = input -'0'; loo=false; break; 
+         case '4': input = input -'0'; loo=false; break; 
+         case '5': input = input -'0'; loo=false; break; 
+         case '6': input = input -'0'; loo=false; break; 
+         case '7': input = input -'0'; loo=false; break; 
+         case '8': input = input -'0'; loo=false; break; 
+         case '9': input = input -'0'; loo=false; break; 
+            }
+    }
+      enlarge(a, input);
    IntoFile.saveImage(a);
    }
 }
