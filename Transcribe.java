0eef2da596f78d6540948897da053ff1eaaada2b
teachmewell <coyu3@duck.com>
2026-08-12 21:41:51 +0200

    Update Transcribe.java

--- Transcribe.java
+++ Transcribe.java
@@ -13,10 +13,10 @@ Predefined.printText(a);*/
 int alen = a.length; int tlen = tem.length; int aglen = a[0].length; int tglen = tem[0].length;  
   int alendif = alen-tlen; // negative if tlen is bigger
   int aglendif = aglen - tglen; // negative if tglen is bigger
-if(alendif > 0){ tem= Img.addRight(tem, alendif); }
-  else{ a = Img.addRight(a, -alendif); }
-if(aglendif > 0){ tem= Img.addDown(tem, aglendif); }
-  else{ a = Img.addDown(a, -aglendif); }
+if(alendif > 0){ tem= Img.addRight(tem, alendif/2); tem=Img.addLeft(tem, alendif-alendif/2); }
+  else{ a = Img.addRight(a, -alendif/2); tem=Img.addLeft(tem, alendif/2-alendif); }
+if(aglendif > 0){ tem= Img.addDown(tem, aglendif/2); tem=Img.addLeft(tem, aglendif-aglendif/2);  }
+  else{ a = Img.addDown(a, -aglendif/2); tem=Img.addLeft(tem, aglendif/2-aglendif);}
   
 for(int y = 0; y<tem[0].length; y++){
   for(int x = 0; x<tem.length; x++){
