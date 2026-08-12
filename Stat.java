7299b708d70524360f1b648a17b91dbd63452792
teachmewell <coyu3@duck.com>
2026-08-12 23:48:53 +0200

    Update Stat.java

--- Stat.java
+++ Stat.java
@@ -20,7 +20,7 @@ for(int x = 0; x< pic.length; x++){
       
        int[][] a = IntoFile.loadImage("drawn.png");
       boolean loo=true;
-      int input = 10;
+      int input = 60;
   
       for(int l = 0; l<args.length; l++){
     if(args[l].equals("-s")){
