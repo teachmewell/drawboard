3258822646336799a011a895f731e4a69f166920
teachmewell <coyu3@duck.com>
2026-08-12 21:29:33 +0200

    Fix variable name typo in Transcribe.java

--- Transcribe.java
+++ Transcribe.java
@@ -10,13 +10,13 @@ Predefined.printText(a);*/
   // Needs a file named template.png  Will copy all red bits into drawn.png as black ones. 
   int[][] a = IntoFile.loadImage("drawn.png");
   int[][] tem = IntoFile.loadImage("template.png");
-int alen = a.length; int tlen = t.length; int aglen = a[0].length; int tglen = tem[0].length;  
+int alen = a.length; int tlen = tem.length; int aglen = a[0].length; int tglen = tem[0].length;  
   int alendif = alen-tlen; // negative if tlen is bigger
   int aglendif = aglen - tglen; // negative if tglen is bigger
 if(alendif > 0){ tem= Img.addRight(tem, alendif); }
   else{ a = Img.addRight(a, alendif); }
 if(aglendif > 0){ tem= Img.addDown(tem, aglendif); }
-  else{ a = Img.addDown(a, algendif); }
+  else{ a = Img.addDown(a, aglendif); }
   
 for(int y = 0; y<tem[0].length; y++){
   for(int x = 0; x<tem.length; x++){
