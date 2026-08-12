95728073a6d77607db4fc6fb1f351e8300edd884
teachmewell <coyu3@duck.com>
2026-08-12 12:22:39 +0200

    Implement printText method in Predefined class
    Added printText method to generate text for a 2D array.

--- Predefined.java
+++ Predefined.java
@@ -1,7 +1,12 @@
 public class Predefined{
 
-  public static void bitcolor (int[][] a){
-    
+  public static void printText (int[][] png){ // Need to put into loop first. Makes text for an array named a[x][y]. 
+    String txt;
+     for (int h=0; h<png[0].length; h++){
+    for(int w=0; w < png.length; w++ ){ 
+        System.out.print( "if(x == "+ w + "&& y =="+ h+"){ a[x][y] = " + png[w][h]+";}" );
+    }
+}
   }
 
   
