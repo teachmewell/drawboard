79fa5c7b4f274e0cd7ee1f74beca9b7a4ef53476
teachmewell <coyu3@duck.com>
2026-08-13 00:04:47 +0200

    Add color inversion functionality to Transcribe

--- Transcribe.java
+++ Transcribe.java
@@ -3,12 +3,16 @@ public class Transcribe{
 
 public static void main(String[] args){
    int color = 0xFF0000;
+   int inverse= 0;
 
    for(int l = 0; l<args.length; l++){
     if(args[l].equals( "-c")){
 if(l+1 >= args.length){System.out.println("You must write a hexadecimal number after -c. Example: \njava Transcribe -c FF00FF"); break;}
              if(args[l+1].startsWith("-") ){System.out.println("You must write a hexadecimal number after -c. Example: \njava Transcribe -c FF00FF"); break;}
             color = Integer.parseInt(args[l+1], 16);
+        }  
+       if(args[l].equals( "-v")){
+            inverse = 1;
         }  
       }
   
@@ -27,8 +31,8 @@ if(aglendif ==0){}
   
 for(int y = 0; y<tem[0].length; y++){
   for(int x = 0; x<tem.length; x++){
-// if (tem[x][y]== color) { a[x][y]= 0xFF0000; }
- if(tem[x][y] == color ){a[x][y] = (~a[x][y]) & 0xFFFFFF;}  //takes the inverse
+ if( inverse==0){if (tem[x][y]== color) { a[x][y]= color; } }
+else{ if(tem[x][y] == color ){a[x][y] = (~a[x][y]) & 0xFFFFFF;} } //takes the inverse
 
 
   }
