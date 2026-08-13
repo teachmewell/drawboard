a0dddd256c14cc969ec8c184b6b91da2268d3dfd
teachmewell <coyu3@duck.com>
2026-08-13 02:49:44 +0200

    Update Img.java

--- Img.java
+++ Img.java
@@ -130,7 +130,7 @@ public class Img extends JPanel {
     }
 //_________________________________________________________________________________________________________________________________________
     public static int[][] addLeft(int[][] png, int bits){
-        int color00 = png[0][0];
+        int color00 = png[0][ png.length / 2];
 int[][] res = new int[png.length + bits][png[0].length];
         for (int h=0; h<png[0].length; h++){
     for(int w=0; w < png.length; w++ ){ 
@@ -156,7 +156,7 @@ int[][] res = new int[png.length - bits][png[0].length];
         return res;
     }//_________________________________________________________________________________________________________________________________________
     public static int[][] addRight(int[][] png, int bits){
-        int color00 = png[0][0];
+        int color00 = png[png.length][png.length / 2];
 int[][] res = new int[png.length + bits][png[0].length];
         for (int h=0; h<png[0].length; h++){
     for(int w=0; w < png.length; w++ ){ 
@@ -194,7 +194,7 @@ int[][] res = new int[png.length][png[0].length - bits];
     }
     //___________________________________________________________________________________________________________________________________
     public static int[][] addUp(int[][] png, int bits){
-         int color00 = png[0][0];
+         int color00 = png[png.length /2][0];
 int[][] res = new int[png.length][png[0].length + bits];
         for (int h=0; h<png[0].length; h++){
     for(int w=0; w < png.length; w++ ){ 
@@ -212,7 +212,7 @@ int[][] res = new int[png.length][png[0].length + bits];
     }
      //___________________________________________________________________________________________________________________________________
     public static int[][] addDown(int[][] png, int bits){
-        int color00 = png[0][0];
+        int color00 = png[png.length/2][png.length];
 int[][] res = new int[png.length][png[0].length + bits];
         for (int h=0; h<png[0].length; h++){
     for(int w=0; w < png.length; w++ ){ 
