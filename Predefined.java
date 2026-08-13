b86f865484783af9ce0051be831333166da7bc6a
teachmewell <coyu3@duck.com>
2026-08-13 12:13:50 +0200

    Rename changeTemplate to hideTemplate and modify logic

--- Predefined.java
+++ Predefined.java
@@ -1,17 +1,17 @@
 public class Predefined{
 
 public static void main(String[] args){
-     int[][] a = IntoFile.loadImage("template.png");
-   printDiffWhite(a);
+     
+  hideTemplate();
 }
    
    //________________________________________________________________________________________________________________________________________-
-public static void changeTemplate(){  //can be main
+public static void hideTemplate(){  //can be main
    int[][] a = IntoFile.loadImage("template.png");
 int[][] n = new int[a.length][a[0].length];
   for (int h=0; h<a[0].length; h++){
     for(int w=0; w < a.length; w++ ){
- if(a[w][h] == 0xFF0000){n[w][h]= 0xFF0000;} else{int g= (int)(Math.random()*0xFFFFFF); if(g!=0xFF0000){n[w][h]= g;} }
+ if(a[w][h] == 0xFF0000){n[w][h]= 0xFF0000;} else{  w[w][h]=0xFF0001;   /*int g= (int)(Math.random()*0xFFFFFF); if(g!=0xFF0000){n[w][h]= g;}*/ }
    }
 }
 IntoFile.saveImage( n,  "template.png" );
