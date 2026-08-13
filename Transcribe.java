576e8b6d976e9529a8319e6217a9fad74374985e
teachmewell <coyu3@duck.com>
2026-08-13 02:46:10 +0200

    Fix increment of loop variable for '-c' argument
    Increment loop variable 'l' after processing '-c' argument.

--- Transcribe.java
+++ Transcribe.java
@@ -10,7 +10,8 @@ public static void main(String[] args){
 if(l+1 >= args.length){System.out.println("You must write a hexadecimal number after -c. Example: \njava Transcribe -c FF00FF"); break;}
              if(args[l+1].startsWith("-") ){System.out.println("You must write a hexadecimal number after -c. Example: \njava Transcribe -c FF00FF"); break;}
             color = Integer.parseInt(args[l+1], 16);
-        }  
+       l++;
+        }
        if(args[l].equals( "-v")){
             inverse = 1;
         }  
