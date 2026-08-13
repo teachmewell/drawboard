c61f6085348db83cdcd99d0ff6587a102aada1ca
teachmewell <coyu3@duck.com>
2026-08-13 02:44:44 +0200

    Update Refactor.java

--- Refactor.java
+++ Refactor.java
@@ -11,32 +11,32 @@ public static void main(String[] args){
 if(l+1 >= args.length){System.out.println("You must write the number of bits to be deleted after -d. Example: \njava Refactor -d 15"); break;}
              if(args[l+1].startsWith("-r") ){System.out.println("You must write the number of bits to be deleted after -d. Example: \njava Refactor -d 15"); break;}
              m =Integer.parseInt(args[l+1]);
-             l=l++;
-       if(m<0){a=Img.deleteDown(a,m);} else{a=Img.addDown(a,m);}
+             l++;
+       if(m<0){a=Img.deleteDown(a,-m);} else{a=Img.addDown(a,m);}
         }
      
       if(args[l].equals( "-u")){
 if(l+1 >= args.length){System.out.println("You must write the number of bits to be deleted after -d. Example: \njava Refactor -u 15"); break;}
              if(args[l+1].startsWith("-l") ){System.out.println("You must write the number of bits to be deleted after -d. Example: \njava Refactor -u 15"); break;}
              m =Integer.parseInt(args[l+1]);
-             l=l++;
-       if(m<0){a=Img.deleteUp(a,m);} else{a=Img.addUp(a,m);}
+             l++;
+       if(m<0){a=Img.deleteUp(a,-m);} else{a=Img.addUp(a,m);}
         }
      
        if(args[l].equals( "-l")){
 if(l+1 >= args.length){System.out.println("You must write the number of bits to be deleted after -d. Example: \njava Refactor -l 15"); break;}
              if(args[l+1].startsWith("-d") ){System.out.println("You must write the number of bits to be deleted after -d. Example: \njava Refactor -l 15"); break;}
              m =Integer.parseInt(args[l+1]);
-             l=l++;
-       if(m<0){a=Img.deleteLeft(a,m);} else{a=Img.addLeft(a,m);}
+             l++;
+       if(m<0){a=Img.deleteLeft(a,-m);} else{a=Img.addLeft(a,m);}
         }
      
        if(args[l].equals( "-r")){
 if(l+1 >= args.length){System.out.println("You must write the number of bits to be deleted after -d. Example: \njava Refactor -r 15"); break;}
              if(args[l+1].startsWith("-u") ){System.out.println("You must write the number of bits to be deleted after -d. Example: \njava Refactor -r 15"); break;}
              m =Integer.parseInt(args[l+1]);
-             l=l++;
-       if(m<0){a=Img.deleteRight(a,m);} else{a=Img.addRight(a,m);}
+             l++;
+       if(m<0){a=Img.deleteRight(a,-m);} else{a=Img.addRight(a,m);}
         }
      
       }
