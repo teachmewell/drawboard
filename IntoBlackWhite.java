e87e8c632e99f1addcc07a0991305bff2582db2a
teachmewell <coyu3@duck.com>
2026-08-13 16:09:36 +0200

    Update IntoBlackWhite.java

--- IntoBlackWhite.java
+++ IntoBlackWhite.java
@@ -1,6 +1,6 @@
 public class IntoBlackWhite{
 
-public static void main(String[][] args){
+public static void main(String[] args){
 int[][] a = IntoFile.loadImage("drawn.png");
   a= Stat.intoBlackWhite(a);
    IntoFile.saveImage(a);
