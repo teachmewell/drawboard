ba05973ffff2661ef6629d612c1875cef8ca15f0
teachmewell <coyu3@duck.com>
2026-08-15 01:01:17 +0200

    Update Stat.java

--- Stat.java
+++ Stat.java
@@ -52,21 +52,19 @@ for(int x = 0; x< pic.length; x++){
     if(args[l].equals("-s")){
        input = Integer.parseInt(args[l+1]);
        l++;
-    }
-          if(args[l].equals("-i")){
-System.out.println("Sizes of drawn.png : x = "+ a.length + " , y = "+ a[0].length);
-             try{  int[][] b = IntoFile.loadImage("template.png");
-                 System.out.println("Sizes of template.png : x = "+ b.length + " , y = "+ b[0].length);}
-             catch ( Exception e ) { return; } 
-return;         
-    }
-      }
-
-      if(input == 0){}
+    if(input == 0){}
      else if(input >0){ a =  enlarge(a, input);} else{a = shrink(a, -input);} 
    IntoFile.saveImage(a);
       
         System.out.println("\n\nProcess of Stat ended. Your image inside drawn.png is now "+ input+" times bigger \n");
+       return;
+    }
+      }
+System.out.println("Sizes of drawn.png : x = "+ a.length + " , y = "+ a[0].length);
+             try{  int[][] b = IntoFile.loadImage("template.png");
+                 System.out.println("Sizes of template.png : x = "+ b.length + " , y = "+ b[0].length);}
+             catch ( Exception e ) { return; } 
+return;  
 }
  //______________________________________________________________________________________________________________________________
 /* public static void main(String[] args) {    // If you use this main: flags -f num .t num  With from color to color change. 
