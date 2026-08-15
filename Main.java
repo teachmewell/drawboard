e3e676631865935213b76418258e1c6e28929cc3
teachmewell <coyu3@duck.com>
2026-08-15 22:50:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import java.awt.image.BufferedImage;
 public class Main {
     public static void main(String[] args) throws IOException {
         
-        int[][] a = new int[120][59]; //was 130 77 before
+        int[][] a = new int[120][60]; //was 130 77 before
         int m= (int) (Math.random() * 0xFF00FF); //color
         boolean autofill = false;
         int brushsize = 1;
