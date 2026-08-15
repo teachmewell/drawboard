7c0b973034f7548c3514bc705a67d3001d3d278b
teachmewell <coyu3@duck.com>
2026-08-15 02:30:10 +0200

    Update Transcribe.java

--- Transcribe.java
+++ Transcribe.java
@@ -1,8 +1,15 @@
+import java.lang.Math;
+
 public class Transcribe{
 
 
-public static void main(String[] args){
+public static void main(String[] args){ 
+   // Needs a file named template.png  Will copy all red bits into drawn.png as black ones. 
+  int[][] a = IntoFile.loadImage("drawn.png");
+  int[][] tem = IntoFile.loadImage("template.png");
+   
    int color = 0xFF0000;
+   boolean resize = true;
    int inverse= 0;
    boolean copyColor = true;
 
@@ -10,7 +17,9 @@ public static void main(String[] args){
  if(args[l].equals( "-b")){
     copyColor = false;
  }
-      
+      if(args[l].equals( "-s")){
+    resize = false;
+ }
     if(args[l].equals( "-c")){
 if(l+1 >= args.length){System.out.println("You must write a hexadecimal number after -c. Example: \njava Transcribe -c FF00FF"); break;}
              if(args[l+1].startsWith("-") ){System.out.println("You must write a hexadecimal number after -c. Example: \njava Transcribe -c FF00FF"); break;}
@@ -22,10 +31,8 @@ if(l+1 >= args.length){System.out.println("You must write a hexadecimal number a
         }  
       }
   
-  // Needs a file named template.png  Will copy all red bits into drawn.png as black ones. 
-  int[][] a = IntoFile.loadImage("drawn.png");
-  int[][] tem = IntoFile.loadImage("template.png");
-
+ 
+if(resize){
 // enlarge the smaller until fits exactly
 if(a.length< tem.length && a[0].length< tem[0].length){
     int enl = Math.max( Math.min(tem.length/ a.length, tem[0].length/ a[0].length), 1 );
@@ -45,9 +52,12 @@ else if(alendif > 0){ tem= Img.addRight(tem, alendif-alendif/2, copyColor); tem=
 if(aglendif ==0){}
  else if(aglendif > 0){ tem= Img.addDown(tem, aglendif/2, copyColor); tem=Img.addUp(tem, aglendif-aglendif/2, copyColor);  }
   else{ a = Img.addDown(a, -aglendif/2, copyColor); a=Img.addUp(a, -aglendif-(-aglendif/2), copyColor);}
-  
-for(int y = 0; y<tem[0].length; y++){
-  for(int x = 0; x<tem.length; x++){
+}
+   //if not resite the smaller one 
+   
+   
+for(int y = 0; y< Math.min(a[0].length, tem[0].length); y++){
+  for(int x = 0; x< Math.min(a.length, tem.length); x++){
  if( inverse==0){if (tem[x][y]== color) { a[x][y]= color; } }
 else{ if(tem[x][y] == color ){a[x][y] = (~a[x][y]) & 0xFFFFFF;} } //takes the inverse
 
